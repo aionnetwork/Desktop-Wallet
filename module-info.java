2db849836bb41219a2a26beaafe7111b55ad9d88
Otniel Nicola <otniel@centrys.io>
2018-03-29 12:14:35 +0300

    Update jars from current dev branch

--- module-info.java
+++ module-info.java
@@ -5,6 +5,7 @@ module aion.wallet{
     requires aion.log;
     requires aion.base;
     requires aion.mcf;
+    requires aion.evtmgr.impl;
     requires javafx.fxml;
     requires javafx.base;
     requires javafx.graphics;
--- src/main/java/org/aion/wallet/WalletApi.java
+++ src/main/java/org/aion/wallet/WalletApi.java
@@ -1,10 +1,29 @@
 package org.aion.wallet;
 
 import org.aion.api.server.ApiAion;
+import org.aion.base.type.ITransaction;
+import org.aion.base.type.ITxReceipt;
+import org.aion.evtmgr.impl.evt.EventTx;
 import org.aion.zero.impl.blockchain.AionImpl;
+import org.aion.zero.impl.types.AionBlockSummary;
 
 public class WalletApi extends ApiAion {
     public WalletApi() {
         super(AionImpl.inst());
+    }
+
+    @Override
+    protected void onBlock(final AionBlockSummary cbs) {
+
+    }
+
+    @Override
+    protected void pendingTxReceived(final ITransaction _tx) {
+
+    }
+
+    @Override
+    protected void pendingTxUpdate(final ITxReceipt _txRcpt, final EventTx.STATE _state) {
+
     }
 }
