c471e5f91739df3acb21f1b29da2d84156c84cc9
Cristian Ilca <i_cristi01@yahoo.com>
2018-04-02 20:56:05 +0300

    AW-40 create account fixes

--- src/main/java/org/aion/wallet/ui/components/partials/AddAccountDialog.java
+++ src/main/java/org/aion/wallet/ui/components/partials/AddAccountDialog.java
@@ -1,10 +1,18 @@
 package org.aion.wallet.ui.components.partials;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Popup;
 import org.aion.mcf.account.Keystore;
 import org.aion.wallet.ui.events.EventBusFactory;
 import org.aion.wallet.ui.events.HeaderPaneButtonEvent;
+
+import java.io.IOException;
 
 public class AddAccountDialog {
 
@@ -13,6 +21,8 @@ public class AddAccountDialog {
 
     @FXML
     private TextField retypedPassword;
+
+    private final Popup popup = new Popup();
 
     public void createAccount() {
         if (newPassword.getText() != null && retypedPassword.getText() != null && newPassword.getText().equals(retypedPassword.getText())) {
@@ -23,5 +33,27 @@ public class AddAccountDialog {
 
     public void unlockAccount() {
 
+    }
+
+    public void open(MouseEvent mouseEvent) {
+        popup.setAutoHide(true);
+        popup.setAutoFix(true);
+
+        Pane addAccountDialog = null;
+        try {
+            addAccountDialog = FXMLLoader.load(getClass().getResource("AddAccountDialog.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Node eventSource = (Node) mouseEvent.getSource();
+        popup.setX(eventSource.getScene().getWindow().getX() + eventSource.getScene().getWidth() / 2 - addAccountDialog.getPrefWidth() / 2);
+        popup.setY(eventSource.getScene().getWindow().getY() + eventSource.getScene().getHeight() / 2 - addAccountDialog.getPrefHeight() / 2);
+        popup.getContent().addAll(addAccountDialog);
+        popup.show(eventSource.getScene().getWindow());
+    }
+
+    public void close() {
+        popup.hide();
     }
 }
