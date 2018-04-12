0e54fbb0d42ff0f4a81edf2bb4ea55b44bf0d849
Otniel Nicola <otniel@centrys.io>
2018-04-12 21:03:32 +0300

    AW-34 - New local storage

---
+++ src/test/java/org/aion/wallet/storage/WalletStorageTest.java
@@ -0,0 +1,78 @@
+package org.aion.wallet.storage;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.*;
+
+public class WalletStorageTest {
+
+    private static final String HOME_DIR = System.getProperty("user.home");
+
+    private static final String STORAGE_DIR = HOME_DIR + File.separator + ".aion";
+
+    private static final String ACCOUNTS_FILE = STORAGE_DIR + File.separator + "accounts.properties";
+
+    private static final String ADDRESS = "some_address";
+
+    private Path file;
+    private Path dir;
+    private WalletStorage walletStorage;
+
+    @Before
+    public void setUp() throws Exception{
+        dir = Paths.get(STORAGE_DIR);
+        file = Paths.get(ACCOUNTS_FILE);
+        assertFalse(Files.exists(dir));
+        assertFalse(Files.exists(file));
+        walletStorage = new WalletStorage();
+        assertTrue(Files.exists(dir));
+        assertTrue(Files.exists(file));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        assertTrue(Files.exists(file));
+        assertTrue(Files.exists(dir));
+        Files.delete(file);
+        Files.delete(dir);
+        assertFalse(Files.exists(file));
+        assertFalse(Files.exists(dir));
+    }
+
+    @Test
+    public void getMissingAccountName() {
+        assertNull(walletStorage.getAccountName(ADDRESS));
+    }
+
+    @Test
+    public void setAccountName() {
+        assertNull(walletStorage.getAccountName(ADDRESS));
+        final String name = "some_name";
+        walletStorage.setAccountName(ADDRESS, name);
+        assertEquals(name, walletStorage.getAccountName(ADDRESS));
+        final String newName = "other_name";
+        walletStorage.setAccountName(ADDRESS, newName);
+        assertEquals(newName, walletStorage.getAccountName(ADDRESS));
+    }
+
+    @Test
+    public void saveAccountName() throws IOException {
+        assertNull(walletStorage.getAccountName(ADDRESS));
+        final String name = "some_name";
+        walletStorage.setAccountName(ADDRESS, name);
+        assertEquals(name, walletStorage.getAccountName(ADDRESS));
+        walletStorage.save();
+
+        WalletStorage newStorage = new WalletStorage();
+        assertEquals(name, newStorage.getAccountName(ADDRESS));
+
+    }
+}
