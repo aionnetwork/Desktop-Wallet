0711a1f572b4e9a27f6514d2f1c719338b8bd5bf
cristi-I-centrys <i_cristi01@yahoo.com>
2018-04-19 16:55:44 +0300

    Added textarea and button for copy to clipboard

--- src/main/java/org/aion/wallet/ui/components/ReceiveController.java
+++ src/main/java/org/aion/wallet/ui/components/ReceiveController.java
@@ -1,12 +1,48 @@
 package org.aion.wallet.ui.components;
 
+import com.google.common.eventbus.Subscribe;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.MouseEvent;
+import org.aion.wallet.dto.AccountDTO;
+import org.aion.wallet.ui.events.EventBusFactory;
+import org.aion.wallet.ui.events.EventPublisher;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ReceiveController implements Initializable{
 
+    @FXML
+    private TextArea accountAddress;
+
+    private AccountDTO accountDTO;
+
     @Override
-    public void initialize(URL location, ResourceBundle resources) {}
+    public void initialize(URL location, ResourceBundle resources) {
+        registerEventBusConsumer();
+    }
+
+    private void registerEventBusConsumer() {
+        EventBusFactory.getBus(EventPublisher.ACCOUNT_CHANGE_EVENT_ID).register(this);
+    }
+
+    @Subscribe
+    private void handleAccountChanged(AccountDTO accountDTO) {
+        this.accountDTO = accountDTO;
+
+        accountAddress.setText(accountDTO.getPublicAddress());
+    }
+
+    public void onCopyToClipBoard(MouseEvent mouseEvent) {
+        if(accountDTO != null && accountDTO.getPublicAddress() != null) {
+            final Clipboard clipboard = Clipboard.getSystemClipboard();
+            final ClipboardContent content = new ClipboardContent();
+            content.putString(accountDTO.getPublicAddress());
+            clipboard.setContent(content);
+        }
+    }
 }
