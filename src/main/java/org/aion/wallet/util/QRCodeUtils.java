784ea9fd6f83d811db27a209bd75790285eb640e
Sergiu Falcusan <sergiu@centrys.io>
2018-06-06 14:41:09 +0300

    QR width and height from method params

--- src/main/java/org/aion/wallet/util/QRCodeUtils.java
+++ src/main/java/org/aion/wallet/util/QRCodeUtils.java
@@ -15,33 +15,31 @@ import java.util.HashMap;
  * -cheers
  */
 public class QRCodeUtils {
-    private static final int QR_WIDTH = 200;
-    private static final int QR_HEIGHT = 200;
     private static final int WHITE = 255 << 16 | 255 << 8 | 255;
     private static final int BLACK = 0;
 
     public static BufferedImage writeQRCode(String content, int width, int height) {
-        BufferedImage image = null;
         try {
-            image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB); // create an empty image
+            BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB); // create an empty image
 
-            HashMap hintMap = new HashMap();
+            HashMap<EncodeHintType, Object> hintMap = new HashMap<>();
             hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.Q);
             hintMap.put(EncodeHintType.MARGIN, 0);
 
             QRCodeWriter writer = new QRCodeWriter();
-            BitMatrix bitMatrix = writer.encode(content, BarcodeFormat.QR_CODE, QR_WIDTH, QR_HEIGHT, hintMap);
-
+            BitMatrix bitMatrix = writer.encode(content, BarcodeFormat.QR_CODE, width, height, hintMap);
 
             for (int i = 0; i < width; i++) {
                 for (int j = 0; j < height; j++) {
                     image.setRGB(i, j, bitMatrix.get(i, j) ? BLACK : WHITE); // set pixel one by one
                 }
             }
+
+            return image;
         } catch (WriterException e) {
             e.printStackTrace();
         }
 
-        return image;
+        return null;
     }
 }
