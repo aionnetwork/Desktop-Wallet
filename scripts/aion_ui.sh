c646a66661e92ff2f5bfac27fe9e5bc66465d91f
Victor Balan <victor.balan93@gmail.com>
2018-06-07 16:00:46 +0300

    AW-103 Add local storage path

--- scripts/aion_ui.sh
+++ scripts/aion_ui.sh
@@ -4,6 +4,6 @@ SCRIPT_PATH=$(dirname $(realpath $0))
 
 pushd ${SCRIPT_PATH}
 
-java -cp "${SCRIPT_PATH}/mod/*:${SCRIPT_PATH}/lib/*" org.aion.wallet.WalletApplication
+java -cp "${SCRIPT_PATH}/mod/*:${SCRIPT_PATH}/lib/*" -Dlocal.storage.dir=${HOME}/.aion org.aion.wallet.WalletApplication
 
 popd
