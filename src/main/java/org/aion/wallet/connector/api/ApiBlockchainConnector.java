09f65160986a429642ed23ea9911534f8eb3cbca
Otniel Nicola <37304045+centrys-otniel@users.noreply.github.com>
2018-06-18 17:18:33 +0300

    AW-119 - Remove old txs before adding new ones (#109)

--- src/main/java/org/aion/wallet/connector/api/ApiBlockchainConnector.java
+++ src/main/java/org/aion/wallet/connector/api/ApiBlockchainConnector.java
@@ -28,7 +28,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.function.Consumer;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.LongStream;
 
@@ -328,17 +327,18 @@ public class ApiBlockchainConnector extends BlockchainConnector {
             if (!addresses.isEmpty()) {
                 final long latest = getLatestBlock().getNumber();
                 final long previousSafe = lastSafeBlock != null ? lastSafeBlock.getNumber() : 0;
-                log.debug("Processing transactions from block: {} for addresses: {}", previousSafe, addresses);
+                log.debug("Processing transactions from block: {} to block: {}, for addresses: {}", previousSafe, latest, addresses);
                 if (previousSafe > 0) {
                     final Block lastSupposedSafe = getBlock(previousSafe);
                     if (!Arrays.equals(lastSafeBlock.getHash(), (lastSupposedSafe.getHash().toBytes()))) {
                         EventPublisher.fireFatalErrorEncountered("A re-organization happened too far back. Please restart Wallet!");
                     }
+                    removeTransactionsFromBlock(addresses, previousSafe);
                 }
                 for (long i = latest; i > previousSafe; i -= BLOCK_BATCH_SIZE) {
                     List<Long> blockBatch = LongStream.iterate(i, j -> j - 1).limit(BLOCK_BATCH_SIZE).boxed().collect(Collectors.toList());
                     List<BlockDetails> blk = getBlockDetailsByNumbers(blockBatch);
-                    blk.forEach(getBlockDetailsConsumer(addresses, previousSafe));
+                    blk.forEach(getBlockDetailsConsumer(addresses));
                 }
                 final long newSafeBlockNumber = latest - BLOCK_BATCH_SIZE;
                 final Block newSafe;
@@ -352,6 +352,23 @@ public class ApiBlockchainConnector extends BlockchainConnector {
             }
         } else {
             log.warn("WIll not process transactions from block: {} for addresses: {} because API is disconnected or no addresses", lastSafeBlock, addresses);
+        }
+    }
+
+    private void removeTransactionsFromBlock(final Set<String> addresses, final long previousSafe) {
+        for (String address : addresses) {
+            final List<TransactionDTO> txs = new ArrayList<>(getAccountManager().getTransactions(address));
+            final Iterator<TransactionDTO> iterator = txs.iterator();
+            final List<TransactionDTO> oldTxs = new ArrayList<>();
+            while (iterator.hasNext()) {
+                final TransactionDTO t = iterator.next();
+                if (t.getBlockNumber() > previousSafe) {
+                    oldTxs.add(t);
+                } else {
+                    break;
+                }
+            }
+            getAccountManager().removeTransactions(address, oldTxs);
         }
     }
 
@@ -370,21 +387,17 @@ public class ApiBlockchainConnector extends BlockchainConnector {
         return txCount;
     }
 
-    private Consumer<BlockDetails> getBlockDetailsConsumer(final Set<String> addresses, final long previousSafe) {
+    private Consumer<BlockDetails> getBlockDetailsConsumer(final Set<String> addresses) {
         return blockDetails -> {
             if (blockDetails != null) {
                 final long timestamp = blockDetails.getTimestamp();
                 final long blockNumber = blockDetails.getNumber();
-                final Predicate<TransactionDTO> isOldTransaction = t -> previousSafe > 0 && t.getBlockNumber() > previousSafe;
                 for (final String address : addresses) {
-                    final List<TransactionDTO> txs = new ArrayList<>(getAccountManager().getTransactions(address));
                     final List<TransactionDTO> newTxs = blockDetails.getTxDetails().stream()
                             .filter(t -> TypeConverter.toJsonHex(t.getFrom().toString()).equals(address)
                                     || TypeConverter.toJsonHex(t.getTo().toString()).equals(address))
                             .map(t -> mapTransaction(t, timestamp, blockNumber))
                             .collect(Collectors.toList());
-                    final List<TransactionDTO> oldTxs = txs.stream().filter(isOldTransaction).collect(Collectors.toList());
-                    getAccountManager().removeTransactions(address, oldTxs);
                     getAccountManager().addTransactions(address, newTxs);
                 }
             }
