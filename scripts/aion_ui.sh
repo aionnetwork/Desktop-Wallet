deb6f7b296f34c27ab50d4fa20b1f205dfd39396
Otniel Nicola <37304045+centrys-otniel@users.noreply.github.com>
2018-06-19 10:48:51 +0300

    Set memory parameters for the java command (#113)

--- scripts/aion_ui.sh
+++ scripts/aion_ui.sh
@@ -5,4 +5,4 @@ SCRIPT_PATH=$(dirname $(realpath $0))
 cd ${SCRIPT_PATH}
 
 STORAGE_DIR=${HOME}/.aion
-java -cp "${SCRIPT_PATH}/mod/*:${SCRIPT_PATH}/lib/*" -Dlocal.storage.dir=${STORAGE_DIR} org.aion.wallet.WalletApplication >> ${STORAGE_DIR}/log_file 2>&1 &
+java -cp "${SCRIPT_PATH}/mod/*:${SCRIPT_PATH}/lib/*" -Dlocal.storage.dir=${STORAGE_DIR} -Xms300m -Xmx500m org.aion.wallet.WalletApplication >> ${STORAGE_DIR}/log_file 2>&1 &
