aa487288d8d3544768ba2993e38f5be9a3fd5bfc
Otniel Nicola <37304045+centrys-otniel@users.noreply.github.com>
2018-06-22 15:18:19 +0300

    GH-116 - Correct use of mnemonic to generate seed for master account (#117)

--- src/main/java/org/aion/wallet/crypto/SeededECKeyEd25519.java
+++ src/main/java/org/aion/wallet/crypto/SeededECKeyEd25519.java
@@ -1,7 +1,6 @@
 package org.aion.wallet.crypto;
 
 import org.aion.crypto.ECKey;
-import org.aion.crypto.ISignature;
 import org.aion.crypto.ed25519.ECKeyEd25519;
 import org.libsodium.jni.Sodium;
 
@@ -9,15 +8,24 @@ import java.math.BigInteger;
 
 public class SeededECKeyEd25519 extends ECKeyEd25519 {
 
+    private static int SIG_BYTES = Sodium.crypto_sign_ed25519_seedbytes();
+
     private final byte[] publicKey;
     private final byte[] secretKey;
     private final byte[] address;
 
     public SeededECKeyEd25519(final byte[] seed) {
+        checkSeed(seed);
         publicKey = new byte[PUBKEY_BYTES];
         secretKey = new byte[SECKEY_BYTES];
         Sodium.crypto_sign_ed25519_seed_keypair(publicKey, secretKey, seed);
         address = computeAddress(publicKey);
+    }
+
+    private void checkSeed(final byte[] seed) {
+        if (SIG_BYTES != seed.length) {
+            throw new IllegalArgumentException(String.format("Seed has to be exactly %s bytes long, but is %s", SIG_BYTES, seed.length));
+        }
     }
 
     @Override
@@ -43,15 +51,5 @@ public class SeededECKeyEd25519 extends ECKeyEd25519 {
     @Override
     public ECKey fromPrivate(final byte[] bs) {
         throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public byte[] computeAddress(final byte[] pubBytes) {
-        return super.computeAddress(pubBytes);
-    }
-
-    @Override
-    public ISignature sign(final byte[] msg) {
-        return super.sign(msg);
     }
 }
