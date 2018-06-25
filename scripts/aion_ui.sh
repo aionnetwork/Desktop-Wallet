edc7df426b1b769802662d03d1ae08f044dbb2e8
Otniel Nicola <37304045+centrys-otniel@users.noreply.github.com>
2018-06-25 15:16:37 +0300

    AW-123 - Remove log redirect from aion_ui.sh (#118)

--- scripts/aion_ui.sh
+++ scripts/aion_ui.sh
@@ -4,5 +4,8 @@ SCRIPT_PATH=$(dirname $(realpath $0))
 
 cd ${SCRIPT_PATH}
 
+MOD_DIR=${SCRIPT_PATH}/mod/*
+LIB_DIR=${SCRIPT_PATH}/lib/*
 STORAGE_DIR=${HOME}/.aion
-java -cp "${SCRIPT_PATH}/mod/*:${SCRIPT_PATH}/lib/*" -Dlocal.storage.dir=${STORAGE_DIR} -Xms300m -Xmx500m org.aion.wallet.WalletApplication >> ${STORAGE_DIR}/log_file 2>&1 &
+
+java -cp "${MOD_DIR}:${LIB_DIR}" -Dlocal.storage.dir=${STORAGE_DIR} -Xms300m -Xmx500m org.aion.wallet.WalletApplication
