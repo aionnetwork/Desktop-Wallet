167d621e81737c0e14136971d3285db34472afa5
Otniel Nicola <otniel@centrys.io>
2018-09-07 10:30:26 +0300

    AW-175 - Fix java evaluation condition

--- scripts/aion_ui.sh
+++ scripts/aion_ui.sh
@@ -7,8 +7,9 @@ cd ${SCRIPT_PATH}
 STORAGE_DIR=${HOME}/.aion
 LOG_DIR=${STORAGE_DIR}/log
 JAVA_INSTALL=${STORAGE_DIR}/jre-10.0.2
+JAVA_CMD=${JAVA_INSTALL}/bin/java
 
-if [ ! -f ${JAVA_INSTALL}/bin/java  ] || [ $(${JAVA_INSTALL}/bin/java -version 2>&1 | grep "10.0.2" | wc -l) <= 0 ]
+if [ ! -f ${JAVA_CMD} ] || [ $(${JAVA_CMD} -version 2>&1 | grep "10.0.2" | wc -l) -lt 1 ]
  then
   mkdir -p ${JAVA_INSTALL}
   wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" \
@@ -23,4 +24,4 @@ LIB_DIR=${SCRIPT_PATH}/lib/*
 
 mkdir -p ${LOG_DIR}
 
-${JAVA_INSTALL}/bin/java -cp "${MOD_DIR}:${LIB_DIR}" -Dlocal.storage.dir=${STORAGE_DIR} -Xms300m -Xmx500m org.aion.wallet.WalletApplication &> ${LOG_DIR}/log
+${JAVA_CMD} -cp "${MOD_DIR}:${LIB_DIR}" -Dlocal.storage.dir=${STORAGE_DIR} -Xms300m -Xmx500m org.aion.wallet.WalletApplication &> ${LOG_DIR}/log &
