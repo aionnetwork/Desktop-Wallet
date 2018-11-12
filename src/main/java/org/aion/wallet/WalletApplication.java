7a66aa870208778dff7b6d49b3370dfa9b0a9b9a
qoire <mr.sun.yao@gmail.com>
2018-11-12 16:06:02 -0500

    minor enhancement to font antialiasing

--- src/main/java/org/aion/wallet/WalletApplication.java
+++ src/main/java/org/aion/wallet/WalletApplication.java
@@ -5,6 +5,9 @@ import org.aion.wallet.util.ConfigUtils;
 
 public class WalletApplication {
     public static void main(String args[]) {
+        // Freebie to get better anti-aliased fonts
+        // see: https://stackoverflow.com/questions/24254000/how-to-force-anti-aliasing-in-javafx-fonts
+        System.setProperty("prism.lcdtext", "false");
         System.setProperty(ConfigUtils.WALLET_API_ENABLED_FLAG, "true");
         javafx.application.Application.launch(MainWindow.class, args);
     }
