7909296a6b22b1e63d77a8d7c2474f3a2a1cb4c0
dennis guye <dennis@aion.network>
2019-04-24 13:10:49 -0400

    Added comments and converted switch-case to if-else

--- src/main/java/org/aion/wallet/connector/AionTransactionSigner.java
+++ src/main/java/org/aion/wallet/connector/AionTransactionSigner.java
@@ -23,46 +23,57 @@ public class AionTransactionSigner {
 
     private final AccountDTO account;
 
-    public AionTransactionSigner(final AccountDTO from) {
+    AionTransactionSigner(final AccountDTO from) {
         account = from;
     }
 
+    /**
+     * Signs the transaction using the accounts private key
+     * @param transaction the transaction to be signed
+     * @return the encoded bytes of a signed transaction
+     * @throws ValidationException
+     * @throws IllegalArgumentException if the account type is not defined
+     */
     public byte[] sign(final AionTransaction transaction) throws ValidationException {
         final AccountType accountType = account.getType();
-        switch (accountType) {
-            case LOCAL:
-            case EXTERNAL:
-                final ECKey ecKey = CryptoUtils.getECKey(account.getPrivateKey());
-                transaction.sign(ecKey);
-                break;
-            case LEDGER:
-            case TREZOR:
-                final HardwareWallet wallet = HardwareWalletFactory.getHardwareWallet(accountType);
-                final AionAccountDetails accountDetails;
-                transaction.setTimeStamp(TimeInstant.now().toEpochMicro());
-                try {
-                    accountDetails = wallet.getAccountDetails(account.getDerivationIndex());
-                } catch (HardwareWalletException e) {
-                    log.error(e.getMessage(), e);
-                    throw new ValidationException(accountType.getDisplayString() + " is not responding. Reconnect!");
-                }
-
-                if (!account.getPublicAddress().equals(accountDetails.getAddress())) {
-                    throw new ValidationException("Wrong " + accountType.getDisplayString() + " connected! Account not found!");
-                }
-
-                final byte[] signature;
-                try {
-                    signature = wallet.signMessage(account.getDerivationIndex(), transaction.getEncodedRaw());
-                    transaction.setSignature(new Ed25519Signature(TypeConverter.StringHexToByteArray(accountDetails.getPublicKey()), signature));
-                } catch (HardwareWalletException e) {
-                    log.error(e.getMessage(), e);
-                    throw new ValidationException(accountType.getDisplayString() + " transaction declined by user");
-                }
-                break;
-            default:
-                throw new IllegalArgumentException("Unsupported account type for transaction signing: " + accountType.getDisplayString());
+        if (accountType == AccountType.LOCAL || accountType == AccountType.EXTERNAL) { // if no HW wallet is available sign the transaction with software
+            signWithDesktopWallet(transaction);
+        } else if (accountType == AccountType.LEDGER || accountType == AccountType.TREZOR) {// if the HW wallet is available sign with the HW wallet
+            signWithHardwareWallet(transaction, accountType);
+        } else { // otherwise throw
+            throw new IllegalArgumentException("Unsupported account type for transaction signing: " + accountType.getDisplayString());
         }
         return transaction.getEncoded();
+    }
+
+    private void signWithDesktopWallet(AionTransaction transaction) {
+        final ECKey ecKey = CryptoUtils.getECKey(account.getPrivateKey());
+        transaction.sign(ecKey);
+    }
+
+    private void signWithHardwareWallet(AionTransaction transaction, AccountType accountType) throws ValidationException {
+
+        final HardwareWallet wallet = HardwareWalletFactory.getHardwareWallet(accountType);
+        final AionAccountDetails accountDetails;
+        transaction.setTimeStamp(TimeInstant.now().toEpochMicro());
+        try {
+            accountDetails = wallet.getAccountDetails(account.getDerivationIndex());
+        } catch (HardwareWalletException e) {
+            log.error(e.getMessage(), e);
+            throw new ValidationException(accountType.getDisplayString() + " is not responding. Reconnect!");
+        }
+
+        if (!account.getPublicAddress().equals(accountDetails.getAddress())) {
+            throw new ValidationException("Wrong " + accountType.getDisplayString() + " connected! Account not found!");
+        }
+
+        final byte[] signature;
+        try {
+            signature = wallet.signMessage(account.getDerivationIndex(), transaction.getEncodedRaw());
+            transaction.setSignature(new Ed25519Signature(TypeConverter.StringHexToByteArray(accountDetails.getPublicKey()), signature));
+        } catch (HardwareWalletException e) {
+            log.error(e.getMessage(), e);
+            throw new ValidationException(accountType.getDisplayString() + " transaction declined by user");
+        }
     }
 }
